bbd1444eb607f1899b132b2ddf67e07c479ae61f
“Thomas <tschoengassner@aidminutes.com>
2021-04-15 13:53:15 +0200

    exercism task pangram

---
+++ flag/elixir/pangram/lib/pangram.ex
@@ -0,0 +1,30 @@
+defmodule Pangram do
+  @doc """
+  Determines if a word or sentence is a pangram.
+  A pangram is a sentence using every letter of the alphabet at least once.
+
+  Returns a boolean.
+
+    ## Examples
+
+      iex> Pangram.pangram?("the quick brown fox jumps over the lazy dog")
+      true
+
+  """
+
+  @spec pangram?(String.t()) :: boolean
+  def pangram?(sentence) do
+    Regex.scan(~r/[a-z]/u, String.downcase(sentence))
+    |> List.flatten()
+    |> Enum.frequencies()
+    |> Map.keys()
+    |> length == 26
+  end
+end
+
+# --- Inspiration
+# just_ascii =
+#   sentence
+#     |> String.to_charlist()
+#     |> Enum.filter(&(&1 in 0..127))
+#     |> List.to_string
