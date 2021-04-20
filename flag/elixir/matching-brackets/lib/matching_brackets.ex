eb612bdf7781a79321eb878e02edda473fe3c363
“Thomas <tschoengassner@aidminutes.com>
2021-04-20 09:57:48 +0200

    solving task matching-brackets

--- flag/elixir/matching-brackets/lib/matching_brackets.ex
+++ flag/elixir/matching-brackets/lib/matching_brackets.ex
@@ -2,7 +2,30 @@ defmodule MatchingBrackets do
   @doc """
   Checks that all the brackets and braces in the string are matched correctly, and nested correctly
   """
+
+  @matching_brackets %{"}" => "{", "]" => "[", ")" => "("}
+
   @spec check_brackets(String.t()) :: boolean
   def check_brackets(str) do
+    Regex.scan(~r/[\{\[\(\)\]\}]/, str)
+    |> List.flatten()
+    |> Enum.reduce_while([], fn k, acc ->
+      cond do
+        k in ["{", "(", "["] ->
+          {:cont, acc ++ [k]}
+
+        k in ["}", ")", "]"] ->
+          if List.last(acc) != @matching_brackets[k],
+            do: {:halt, false},
+            else: {:cont, List.delete_at(acc, -1)}
+
+        true ->
+          {:cont, acc}
+      end
+    end)
+    |> (&==/2).([])
   end
 end
+
+# ---------- Inspiration
+# check_brackets=~r/((\((?:[^\{\})(\[\]]+|(?R))*+\))|(\[(?:[^\{\})(\[\]]+|(?R))*+\])|(\{(?:[^\{\})(\[\]]+|(?R))*+\}))/
