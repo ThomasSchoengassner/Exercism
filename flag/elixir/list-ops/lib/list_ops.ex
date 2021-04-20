9ce71209b4432b5310c763b288b6d5a10353b48f
“Thomas <tschoengassner@aidminutes.com>
2021-04-20 09:59:59 +0200

    exercism task list-ops

---
+++ flag/elixir/list-ops/lib/list_ops.ex
@@ -0,0 +1,76 @@
+defmodule ListOps do
+  # Please don't use any external modules (especially List or Enum) in your
+  # implementation. The point of this exercise is to create these basic
+  # functions yourself. You may use basic Kernel functions (like `Kernel.+/2`
+  # for adding numbers), but please do not use Kernel functions for Lists like
+  # `++`, `--`, `hd`, `tl`, `in`, and `length`.
+
+  defp elements([_head | tail], n), do: elements(tail, n + 1)
+  defp elements([], n), do: n
+
+  @spec count(list) :: non_neg_integer
+  def count(l) do
+    elements(l, 0)
+  end
+
+  defp mirror([head | tail], list), do: mirror(tail, [head | list])
+  defp mirror([], list), do: list
+
+  @spec reverse(list) :: list
+  def reverse(l) do
+    mirror(l, [])
+  end
+
+  @spec map(list, (any -> any)) :: list
+  def map(l, f) do
+    for x <- l do
+      f.(x)
+    end
+  end
+
+  defp check([], _f, n), do: n
+
+  defp check(l, f, n) do
+    [x | rest] = l
+    n = if f.(x), do: [x | n], else: n
+    check(rest, f, n)
+  end
+
+  @spec filter(list, (any -> as_boolean(term))) :: list
+  def filter(l, f) do
+    check(l, f, [])
+    |> mirror([])
+  end
+
+  @spec aggregate(list, any, any) :: any
+  defp aggregate([head | tail], acc, f), do: aggregate(tail, f.(head, acc), f)
+  defp aggregate([], acc, _), do: acc
+
+  def reduce(l, acc, f) do
+    aggregate(l, acc, f)
+  end
+
+  defp add_items(a, [head | tail]), do: add_items([head | a], tail)
+  defp add_items(a, []), do: a
+
+  def append(a, b) do
+    a
+    |> mirror([])
+    |> add_items(b)
+    |> mirror([])
+  end
+
+  defp concat(list, [head|tail]) do
+    case is_list(head) do
+      true -> append(list,head)
+      false -> [head | list]
+    end
+    |> concat(tail)
+  end
+  defp concat(list, []),do: list
+
+  @spec concat([[any]]) :: [any]
+  def concat(ll) do
+    concat([], ll)
+  end
+end
